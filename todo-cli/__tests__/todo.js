4ec53350edbea4a88275f1b371efc8d9a4f325c6
Nanda2361 <145745684+Nanda2361@users.noreply.github.com>
2023-12-20 08:01:08 -0800

    NK

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,65 +1,77 @@
 const todoList = require("../todo");
 
 describe("TodoList Test Suite", () => {
-  let nandatodo;
+  let todo;
+
   beforeEach(() => {
-    nandatodo = todoList();
+    todo = todoList();
   });
 
-  test("Should create a new todo", () => {
-    nandatodo.add({
+  test("We Should create a new todo", () => {
+    todo.add({
       title: "New Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
-    expect(nandatodo.all.length).toBe(1);
-    expect(nandatodo.all[0].title).toBe("New todo");
+
+    expect(todo.all.length).toBe(1);
+    expect(todo.all[0].title).toBe("New Todo");
   });
 
-  test("Should mark a todo as completed", () => {
-    nandatodo.add({
+  test("Should mark a todo as completed after its completion", () => {
+    todo.add({
       title: "Incomplete Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
-    nandatodo.markAsComplete(0);
-    expect(nandatodo.all[0].completed).toBe(true);
+
+    todo.markAsComplete(0);
+
+    expect(todo.all[0].completed).toBe(true);
   });
 
+  test("Should retrieve overdue items after the time is over", () => {
+    
 
-  test("Should retrieve overdue items", () => {
-    const today = new Date().toISOString().split("T")[0];
-    nandatodo.add({
+    todo.add({
       title: "Overdue Todo",
       completed: false,
       dueDate: "2023-01-01",
     });
-    const ushaoverdue = nandatodo.overdue();
-    expect(ushaoverdue.length).toBe(1);
-    expect(ushaoverdue[0].title).toBe("Overdue todo");
+
+    const ThingsallOverDue = todo.overdue();
+
+    expect(ThingsallOverDue.length).toBe(1);
+    expect(ThingsallOverDue[0].title).toBe("Overdue Todo");
   });
 
-  test("Should retrieve due today items", () => {
-    const today = new Date().toISOString().split("T")[0];
-    nandatodo.add({
+  test("Should retrieve due today items which are mentioned", () => {
+    const todayDate = new Date().toISOString().split("T")[0];
+
+    todo.add({
       title: "Due Today Todo",
       completed: false,
       dueDate: todayDate,
     });
-    const laddudueToday = nandatodo.dueToday();
-    expect(laddudueToday.length).toBe(1);
-    expect(laddudueToday[0].title).toBe("Due Today Todo");
+
+    const ThingstodayDue = todo.dueToday();
+
+    expect(ThingstodayDue.length).toBe(1);
+    expect(ThingstodayDue[0].title).toBe("Due Today Todo");
   });
 
   test("Should retrieve due later items", () => {
-    const today = new Date().toISOString().split("T")[0];
-    nandatodo.add({
+    
+
+    todo.add({
       title: "Due Later Todo",
       completed: false,
       dueDate: "2023-12-31",
     });
-    const kishorduelater = nandatodo.dueLater();
-    expect(kishorduelater.length).toBe(1);
-    expect(kishorduelater[0].title).toBe("Due Later Todo");
+
+    const ThingsdueLater = todo.dueLater();
+
+    expect(ThingsdueLater.length).toBe(1);
+    expect(ThingsdueLater[0].title).toBe("Due Later Todo");
   });
 });
